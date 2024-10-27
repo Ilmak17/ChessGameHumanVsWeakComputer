e5fc2fd5c316a8b2337cf9e85a2a2581236944f7
Ilyas Makirov <ilmak1704@gmail.com>
2024-10-27 15:55:48 +0500

    refactoring and bug fixing

--- src/com/game/chess/pieces/Movement.java
+++ src/com/game/chess/pieces/Movement.java
@@ -1,7 +1,6 @@
 package com.game.chess.pieces;
 
 public interface Movement {
-
     boolean isValidMove(Position destPosition);
     void move(Position position);
     void forceMove(Position position);
